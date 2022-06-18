5dce4f39a0b325fcdbd0399cb7bce0dfa8a30dda
iYasha <33287747+iYasha@users.noreply.github.com>
2022-06-18 15:08:56 +0300

    Lesson 18.06

--- todo/settings.py
+++ todo/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'articles.apps.ArticlesConfig',
     'task.apps.TaskConfig',
 
     'django.contrib.admin',
--- todo/urls.py
+++ todo/urls.py
@@ -19,6 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('articles/', include('articles.urls')),
     path('tasks/', include('task.urls')),
     path('admin/', admin.site.urls),
 ]
